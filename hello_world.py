9d5a3eeca90e24f3de480592495ac86f159d5716
John Rodi <77717473+JohnGabrielRodi@users.noreply.github.com>
2021-01-20 04:52:27 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,20 +1,40 @@
- # Master index of hello world program.
+# Master index of hello world program.
 
- class Hello:
- print('Beginning Hello_world program...')
- 
- def __init__(self, hello_string):
-     self.hello_string = hello_string
+class Hello:
+    print('Beginning Hello_world program...')
   
- def method(self):
+  def __init__(self, hello_string, split_up):
+      self.hello_string = hello_string
+      self.split_up = split_up
+
+  def method(self):
       # repository for odds & ends
-      pass
+      print(self.split_up)
 
- def print_hw(self):
-     ''' simple print method '''
-     print(self.hello_string)
+  def print_hw(self):
+      ''' simple print method '''
+      print(self.hello_string)
+
+  def shuffles(self):
+      '''  splits and returns '''
+      import random
+      bar = self.hello_string.split()
+      return bar
+
+  def with_str(self):
+      ''' returns after string '''
+      print('result:', self.hello_string)
+
+  def multiples(self):
+      '''  returns multiples '''
+      return self.hello_string * 4
+
+  def comp(self):
+      ''' splits and returns through a list comprehension '''
+      bar = [x for x in self.hello_string]
+      print(bar)
+
 
     
-    
-obj = Hello('Hello World!')
-obj.print_hw()
+
+obj = Hello('Hello World!', ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'])
