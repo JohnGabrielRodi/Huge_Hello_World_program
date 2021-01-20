0df512e5281588044e83871914551f195962ba82
John Rodi <77717473+JohnGabrielRodi@users.noreply.github.com>
2021-01-20 03:08:42 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,20 +1,20 @@
  # Master index of hello world program.
 
-  class Hello:
-    print('Beginning Hello_world program...')
-
-    def __init__(self, hello_string):
-      self.hello_string = hello_string
-
-    def method(self):
+ class Hello:
+ print('Beginning Hello_world program...')
+ 
+ def __init__(self, hello_string):
+     self.hello_string = hello_string
+  
+ def method(self):
       # repository for odds & ends
       pass
 
-    def print_hw(self):
-      ''' simple print method '''
-      print(self.hello_string)
+ def print_hw(self):
+     ''' simple print method '''
+     print(self.hello_string)
 
     
-
+    
 obj = Hello('Hello World!')
 obj.print_hw()
