91a8caeafd1a56433e63de949097e7052ebce873
John Rodi <77717473+JohnGabrielRodi@users.noreply.github.com>
2021-01-20 02:38:38 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,11 @@
 # Master index of hello world program.
+
+class Hello_world:
+  print('Beginning Hello_world program...')
+
+  def __init__(self):
+    pass
+
+  def method(self):
+    # repository for odds & ends
+    pass
